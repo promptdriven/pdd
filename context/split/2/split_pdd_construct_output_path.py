74d3a92744078c753942ab9ed6312fba007d702a
Greg Tanaka <glt@alumni.caltech.edu>
2024-08-01 15:21:51 -0700

    requirements generation work

--- context/split/2/split_pdd_construct_output_path.py
+++ context/split/2/split_pdd_construct_output_path.py
@@ -1,6 +1,6 @@
 runnable_output_path, example_output_path = construct_output_paths(
     basename, # the basename of the file
     file_extension, # the file extension based on the language
-    argv_output_path # string from the '-o' flag
+    argv_output_path, # string from the '-o' flag
     argv_example_output_path # string from the '-oe' flag
     )
