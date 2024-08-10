f196544eb628cc710d01586382027000d1119de5
Greg Tanaka <glt@alumni.caltech.edu>
2024-08-10 15:39:49 -0700

    another split example

---
+++ context/split/3/split_postprocess_find_section.py
@@ -0,0 +1,5 @@
+sections = find_section(
+    lines, # splitlines of text, an array of lines of text
+    start_index=0, # start index/row of the line, default is row 0
+    sub_section=False # boolean to check if it is a sub-section, default is no
+    )
