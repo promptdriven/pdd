61190e03007350a65600dff265fa4b557de45619
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-09-29 16:56:42 -0400

    remove accidentally pushed dummy files

--- agentic_fallback_example/src/utils.py
+++
@@ -1,6 +0,0 @@
-# This file defines the utility function.
-# The agentic fallback will need to read this file to fix the bug in main.py.
-
-def get_greeting(first_name: str, last_name: str) -> str:
-    """Returns a greeting string."""
-    return f"Hello, {first_name} {last_name}!"
