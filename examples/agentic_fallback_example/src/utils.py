c89cb21c81430af325ba938b1c54c25408c48b28
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-10-07 00:58:11 -0400

    enable multi file editing function for different languages

--- examples/agentic_fallback_example/src/utils.py
+++ examples/agentic_fallback_example/src/utils.py
@@ -1,6 +1,3 @@
-# This file defines the utility function.
-# The agentic fallback will need to read this file to fix the bug in main.py.
-
 def get_greeting(first_name: str, last_name: str) -> str:
-    """Returns a greeting string."""
+    """Returns a greeting for a user."""
     return f"Hello, {first_name} {last_name}!"
