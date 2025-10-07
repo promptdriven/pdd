ddb1c8ea2e64118e4f0a48b9615b20471c6278c9
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-10-07 00:58:11 -0400

    enable multi file editing function for different languages

--- examples/agentic_fallback_example_java/src/Main.java
+++ examples/agentic_fallback_example_java/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Intentionally relies on Utils.add(int, int), but Utils.java is wrong.
-        int result = Utils.add(2, 3);
-        System.out.println(result);
+        System.out.println(Utils.add(2, 3));
     }
 }
