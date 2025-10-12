5350f18fcf2d103f129b7f7bde4cd404149d18d7
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-10-12 18:54:39 -0400

    pass maven test

--- examples/agentic_fallback_example_java_maven/src/main/java/Main.java
+++ examples/agentic_fallback_example_java_maven/src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Sum: " + Util.add(2, 3));
-    }
-
-    public static int computeSomething(int x) {
-        return Util.add(x, 5);
+    public static int xPlus3(int x) {
+        return Util.add(x, 3);
     }
 }
