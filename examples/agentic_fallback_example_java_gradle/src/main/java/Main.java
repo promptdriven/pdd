77e793bd745e809b75615f00d99b163e44baa577
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-10-12 20:11:08 -0400

    update harvest only prompt to increase success rate

--- examples/agentic_fallback_example_java_gradle/src/main/java/Main.java
+++ examples/agentic_fallback_example_java_gradle/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static int computeSomething(int x) {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main!");
+    }
+    
+    public static int testComputeSomething(int x) {
         return Util.add(x, 3);
     }
 }
