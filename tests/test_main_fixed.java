f14b9e707fb4d1d36c659a028acda5c0d68eb797
Cai Jiamin <caijiamin@caideair.mynetworksettings.com>
2025-10-12 22:40:50 -0400

    delete dummy tests

--- tests/test_main_fixed.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-public class TestMain {
-    @Test
-    void testComputeSomething() {
-        assertEquals(5, Main.testComputeSomething(2));
-    }
-}
