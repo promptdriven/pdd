d40f792775b0cd7609dcb0451ad6d0b123f3745c
Avantika Jalote <ajalote2@illinois.edu>
2025-11-24 09:44:51 -0600

    Remove examples/hello/hello.py from PR

--- examples/hello/hello.py
+++
@@ -1,8 +0,0 @@
-def hello() -> None:
-    """This function prints the string 'hello' to the console."""
-    print("hello")
-
-
-if __name__ == "__main__":
-    # To use the function, you must call it:
-    hello()
