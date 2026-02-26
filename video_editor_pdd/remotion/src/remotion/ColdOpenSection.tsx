6ffe941bcb0b08e6edb9e379a4e827aa0d046802
Greg Tanaka <glt@alumni.caltech.edu>
2026-02-26 11:02:31 -0800

    fix(test-batch-ann-1772132467714): Subtitle font size 96px causes text to clip the ri

--- video_editor_pdd/remotion/src/remotion/ColdOpenSection.tsx
+++ video_editor_pdd/remotion/src/remotion/ColdOpenSection.tsx
@@ -43,13 +43,15 @@ const Subtitles: React.FC = () => {
           color: "white",
           padding: "12px 24px",
           borderRadius: 8,
-          fontSize: 36,
+          fontSize: 48,
           fontFamily: "sans-serif",
           maxWidth: "80%",
           textAlign: "center",
           overflowWrap: "break-word",
           wordBreak: "break-word",
           overflow: "hidden",
+          textOverflow: "ellipsis",
+          boxSizing: "border-box",
         }}
       >
         {text}
