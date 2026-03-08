3b4218c33be809b164c73cf3534e707d19ca17bc
Greg Tanaka <glt@alumni.caltech.edu>
2026-03-07 23:58:04 -0800

    pre-fix snapshot: section section-1, batch job-resolve-batch-001

--- video_editor_pdd/remotion/src/remotion/Root.tsx
+++ video_editor_pdd/remotion/src/remotion/Root.tsx
@@ -1,84 +1,9 @@
 import React from "react";
 import { Composition } from "remotion";
 
-import { AnimationSectionSection } from "./animation_section";
-import { VeoSectionSection } from "./veo_section";
-import { AnimationSection01TitleCard } from "./AnimationSection01TitleCard";
-import { AnimationSection02KeyVisual } from "./animation_section_02_key_visual";
-import { AnimationSection03SplitSummary } from "./animation_section_03_split_summary";
-import { VeoSection01TitleCard } from "./veo_section_01_title_card";
-import { VeoSection02KeyVisual } from "./veo_section_02_key_visual";
-import { VeoSection03SplitSummary } from "./veo_section_03_split_summary";
-
-const PREVIEW_DURATION = 150; // 5s at 30fps
-
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      <Composition
-        id="AnimationSection"
-        component={AnimationSectionSection}
-        durationInFrames={220}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="VeoSection"
-        component={VeoSectionSection}
-        durationInFrames={221}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="animation-section01-title-card"
-        component={AnimationSection01TitleCard}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="animation-section02-key-visual"
-        component={AnimationSection02KeyVisual}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="animation-section03-split-summary"
-        component={AnimationSection03SplitSummary}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="veo-section01-title-card"
-        component={VeoSection01TitleCard}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="veo-section02-key-visual"
-        component={VeoSection02KeyVisual}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-      <Composition
-        id="veo-section03-split-summary"
-        component={VeoSection03SplitSummary}
-        durationInFrames={PREVIEW_DURATION}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
     </>
   );
 };
