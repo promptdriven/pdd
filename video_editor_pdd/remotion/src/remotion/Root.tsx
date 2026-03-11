49199779afc26df475964a9af1f910a172aad3d5
Greg Tanaka <glt@alumni.caltech.edu>
2026-03-11 00:43:11 -0700

    chore: pre-fix snapshot for animation_section (434bbe55-31ce-4e65-8382-43dd4416f93e)

--- video_editor_pdd/remotion/src/remotion/Root.tsx
+++ video_editor_pdd/remotion/src/remotion/Root.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import { Composition } from "remotion";
+import "./_shared/load-inter-font";
+
+import { AnimationSectionSection } from "./animation_section";
+import { VeoSectionSection } from "./veo_section";
+
+const PREVIEW_DURATION = 150; // 5s at 30fps
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      <Composition
+        id="AnimationSection"
+        component={AnimationSectionSection}
+        durationInFrames={360}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="VeoSection"
+        component={VeoSectionSection}
+        durationInFrames={360}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
     </>
   );
 };
