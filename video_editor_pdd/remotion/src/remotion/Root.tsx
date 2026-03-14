8089bc3ead2557c813e0a0b49e07e8b4df852893
Greg Tanaka <glt@alumni.caltech.edu>
2026-03-13 22:38:46 -0700

    chore: pre-fix snapshot for animation_section (01d6fa60-c45b-4eb3-b8fb-d378d246cecc)

--- video_editor_pdd/remotion/src/remotion/Root.tsx
+++ video_editor_pdd/remotion/src/remotion/Root.tsx
@@ -67,7 +67,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="AnimationSection"
         component={AnimationSectionSection}
-        durationInFrames={228}
+        durationInFrames={230}
         fps={30}
         width={1920}
         height={1080}
@@ -75,7 +75,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="VeoSection"
         component={VeoSectionSection}
-        durationInFrames={229}
+        durationInFrames={231}
         fps={30}
         width={1920}
         height={1080}
