37569c852ab43a0a81f03a3e1eb84d3accc75dca
Greg Tanaka <glt@alumni.caltech.edu>
2026-03-17 23:30:01 -0700

    Bump version

--- pdd/__init__.py
+++ pdd/__init__.py
@@ -2,7 +2,7 @@
 
 import os
 
-__version__ = "0.0.179"
+__version__ = "0.0.180"
 
 # Strength parameter used for LLM extraction across the codebase
 # Used in postprocessing, XML tagging, code generation, and other extraction
