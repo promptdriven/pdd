ebf4c52cfcf2ab55ce6552d4b290f891adcd4f92
Vishal Ramvelu <rv.vishal@gmail.com>
2026-04-04 15:08:43 -0500

    chore: drop stray core_dump_smoke, context simple_math example, and uv.lock

--- context/simple_math_example.py
+++
@@ -1,8 +0,0 @@
-# Assuming the test module is saved as 'test_add.py' and 'solution.py' contains:
-# def add(a, b):
-#     return a + b
-
-# To run the tests:
-if __name__ == "__main__":
-    import pytest
-    pytest.main(['test_add.py', '-v'])
